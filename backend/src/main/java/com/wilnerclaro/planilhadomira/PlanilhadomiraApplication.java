656c5f17d9f5f7cf18faab9f71ea9976a2176691
wilnerclaro <wilnerclaro@gmail.com>
2021-09-09 17:46:26 -0300

    Project created

---
+++ backend/src/main/java/com/wilnerclaro/planilhadomira/PlanilhadomiraApplication.java
@@ -0,0 +1,13 @@
+package com.wilnerclaro.planilhadomira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanilhadomiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanilhadomiraApplication.class, args);
+	}
+
+}
